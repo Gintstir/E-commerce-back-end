ef7a7035e9f52c36fd82cea8f18675fab0d95b12
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-01-28 15:25:25 -0500

    finished building Product  model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: Datatypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: Datatypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: Datatypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
